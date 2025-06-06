4e219c5b4a0515c670013ae98fddf9c23abb15a1
Yodaman97 <yodaman97@gmail.com>
2025-06-06 17:48:16 -0400

    Customize branding for Holiness Stream

--- ui/src/common/Title.jsx
+++ ui/src/common/Title.jsx
@@ -8,7 +8,7 @@ export const Title = ({ subTitle, args }) => {
   const text = translate(subTitle, { ...args, _: subTitle })
 
   if (isDesktop) {
-    return <span>Navidrome {text ? ` - ${text}` : ''}</span>
+    return <span>Holiness Stream {text ? ` - ${text}` : ''}</span>
   }
-  return <span>{text ? text : 'Navidrome'}</span>
+  return <span>{text ? text : 'Holiness Stream'}</span>
 }
